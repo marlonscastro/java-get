f80bcaba18e172dfac4335c0640b792eb583923e
marlonscastro <marloncastro@live.com>
2020-09-04 16:16:25 -0300

    ajustes no bairro

--- src/Mapa.java
+++ src/Mapa.java
@@ -10,12 +10,16 @@ import org.json.JSONObject;
 public class Mapa {
 	private final String USER_AGENT = "Mozilla/5.0";
 	public String bairro = "";
+	
+	private String urlBase = "https://mapas.pe.gov.br";
 
 	protected Map<String, String> get(String lat, String lon) throws Exception {
 		Map<String, String> data = new HashMap<String, String>();
 
-		String url = "https://mapas.pe.gov.br/nominatim/reverse?format=json&lat=" + lat + "&lon=" + lon
-				+ "&zoom=14&addressdetails=1";
+		String url = this.urlBase + 
+				"/nominatim/reverse?format=json&lat=" + 
+				lat + "&lon=" + 
+				lon + "&zoom=14&addressdetails=1";
 		URL obj = new URL(url);
 
 		HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
@@ -23,9 +27,8 @@ public class Mapa {
 		conn.setRequestMethod("GET");
 		conn.setRequestProperty("User-Agent", USER_AGENT);
 
-		int responseCode = conn.getResponseCode();
-
-		System.out.println("Response Code: " + responseCode);
+//		int responseCode = conn.getResponseCode();
+//		System.out.println("Response Code: " + responseCode);
 
 		BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 
@@ -39,10 +42,15 @@ public class Mapa {
 		JSONObject json = new JSONObject(response.toString());
 		JSONObject endereco = (JSONObject) json.get("address");
 
-		data.put("bairro", endereco.get("suburb").toString());
-		data.put("municipio", endereco.get("city").toString());
+		try {
+			this.bairro = endereco.get("suburb").toString();
+		} catch (Exception e) {
+			this.bairro = "Centro";
+		}
+		
+		data.put("bairro", this.bairro);
+		data.put("municipio", endereco.get("city_district").toString());
 
 		return data;
-
 	}
 }
--- src/MapasTest.java
+++ src/MapasTest.java
@@ -4,13 +4,15 @@ public class MapasTest {
 	public static void main(String[] args) {
 		try {
 			Mapa mapa = new Mapa();
-//			Map<String, String> dados = mapa.get("-8.0314895", "-34.9375628");
-			Map<String, String> dados = mapa.get("-8.0268232", "-34.9499354");
+			Map<String, String> dados = null;
+//			dados = mapa.get("-8.0314895", "-34.9375628");   // Iputinga
+//			dados = mapa.get("-8.0268232", "-34.9499354");   // Nova Morada
+			dados = mapa.get("-7.50272635", "-35.3132763");  
 			
 			System.out.println("Bairro: " + dados.get("bairro"));
 			System.out.println("Municipio: " + dados.get("municipio"));			
 		} catch (Exception e) {
-			System.err.println("Exceção. trace: " + e);
+			System.err.println("Deu merda. trace: " + e);
 		}
 	}
 }
